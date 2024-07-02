dfe44629c6ba0296a729feee3b345f0c9f78f3ad
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-07-03 03:31:48 +0700

    Request Param

--- src/main/java/com/spring/mvc/controller/HelloController.java
+++ src/main/java/com/spring/mvc/controller/HelloController.java
@@ -1,13 +1,11 @@
 package com.spring.mvc.controller;
 
 import com.spring.mvc.service.HelloService;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
 
@@ -19,9 +17,8 @@ public class HelloController {
 
 //    @RequestMapping(path = "/hello", method = RequestMethod.GET)
     @GetMapping(path = "/hello")
-    public void helloWorld(HttpServletResponse response,
-                           HttpServletRequest request) throws IOException {
-        String name = request.getParameter("name");
+    public void helloWorld(@RequestParam(name = "name", required = false) String name,
+                           HttpServletResponse response) throws IOException {
         String responseBody = helloService.hello(name);
         response.getWriter().println(responseBody);
     }
