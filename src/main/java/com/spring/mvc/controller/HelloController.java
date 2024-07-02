6925ba1b0735f798950d2929298c31d2de9f2264
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-07-03 02:55:27 +0700

    Mock Bean

--- src/main/java/com/spring/mvc/controller/HelloController.java
+++ src/main/java/com/spring/mvc/controller/HelloController.java
@@ -1,23 +1,24 @@
 package com.spring.mvc.controller;
 
+import com.spring.mvc.service.HelloService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.IOException;
-import java.util.Objects;
 
 @Controller
 public class HelloController {
 
+    @Autowired
+    private HelloService helloService;
+
     @RequestMapping(path = "/hello")
     public void helloWorld(HttpServletResponse response, HttpServletRequest request) throws IOException {
         String name = request.getParameter("name");
-        if (Objects.isNull(name)) {
-            name = "Guest";
-        }
-
-        response.getWriter().println("Hello " + name);
+        String responseBody = helloService.hello(name);
+        response.getWriter().println(responseBody);
     }
 }
