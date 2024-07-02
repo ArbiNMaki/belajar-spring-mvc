ee599f82ae53315a1245355db0314a597df5d7b3
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-07-03 00:32:50 +0700

    Integration Test

---
+++ src/test/java/com/spring/mvc/controller/HelloControllerIntegrationTest.java
@@ -0,0 +1,36 @@
+package com.spring.mvc.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloControllerIntegrationTest {
+
+    @LocalServerPort
+    private Integer port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void helloGuest() {
+        String response = restTemplate
+                .getForEntity("http://localhost:" + port + "/hello", String.class)
+                .getBody();
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals("Hello Guest", response.trim());
+    }
+
+    @Test
+    void helloName() {
+        String response = restTemplate
+                .getForEntity("http://localhost:" + port + "/hello?name=Arbi", String.class)
+                .getBody();
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals("Hello Arbi", response.trim());
+    }
+}
