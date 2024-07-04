57ca7a1c9da26db497f442e4b4a12504df54612a
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-07-05 03:18:42 +0700

    Response Entity

---
+++ src/main/java/com/spring/mvc/controller/AuthController.java
@@ -0,0 +1,23 @@
+package com.spring.mvc.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthController {
+
+    @PostMapping(path = "/auth/login",
+                 consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity<String> login(@RequestParam(name = "username") String username,
+                                        @RequestParam(name = "password") String password) {
+        if ("arbi".equals(username) && "rahasia".equals(password)) {
+            return ResponseEntity.status(HttpStatus.OK).body("OK");
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("KO");
+        }
+    }
+}
