64ec2d13b3f12093fa3cc279339b226f40f57164
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-07-05 07:03:42 +0700

    JSON

---
+++ src/main/java/com/spring/mvc/controller/PersonApiController.java
@@ -0,0 +1,24 @@
+package com.spring.mvc.controller;
+
+import com.spring.mvc.model.CreatePersonRequest;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.print.attribute.standard.Media;
+
+@Controller
+public class PersonApiController {
+
+    @PostMapping(
+            path = "/api/person",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseBody
+    public CreatePersonRequest createPersonRequest(@RequestBody CreatePersonRequest request) {
+        return request;
+    }
+}
