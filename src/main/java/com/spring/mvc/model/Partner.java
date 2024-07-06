571b83d02b72088e11f8aaca84dc7a28713b0907
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-07-06 08:05:04 +0700

    Argument Resolver

---
+++ src/main/java/com/spring/mvc/model/Partner.java
@@ -0,0 +1,15 @@
+package com.spring.mvc.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Partner {
+
+    private String id;
+
+    private String name;
+}
